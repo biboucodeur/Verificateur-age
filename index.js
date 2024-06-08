1026b68ee179b46f4adcfaa6c810341151267d7a
Abibouwade <abibouwade2298@gmail.com>
2024-06-08 16:08:38 +0000

    V2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function checkAge() {
-    var birthdateInput = document.getElementById("birthdate").value;
-    var birthdate = new Date(birthdateInput);
-    var today = new Date();
-    var age = today.getFullYear() - birthdate.getFullYear();
-    var monthDiff = today.getMonth() - birthdate.getMonth();
+    let birthdateInput = document.getElementById("birthdate").value;
+    let birthdate = new Date(birthdateInput);
+    let today = new Date();
+    let age = today.getFullYear() - birthdate.getFullYear();
+    let monthDiff = today.getMonth() - birthdate.getMonth();
     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthdate.getDate())) {
         age--;
     }
 
-    var resultElement = document.getElementById("result");
+    let resultElement = document.getElementById("result");
     if (isNaN(age)) {
         resultElement.textContent = "Veuillez entrer une date de naissance valide.";
     } else if (age >= 18) {
